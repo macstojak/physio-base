3c7b6a79f53912a8bcfe222f57bf5bec90062e10
Maciek <mac.skot@wp.pl>
2018-01-23 11:52:57 +0000

    add app.js, footer and header

---
+++ app.js
@@ -0,0 +1,42 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    // Campground  = require("./models/campground"),
+    // Comment     = require("./models/comment"),
+    // User        = require("./models/user"),
+    flash       = require("connect-flash")
+    
+//routes in here VVVV    
+    
+mongoose.connect(process.env.DATABASEURL);
+
+app.use(bodyParser.urlencoded({extended: true})); //
+app.set("view engine", "ejs"); //parse ejs files extentions
+app.use(express.static(__dirname+"/public")); 
+app.use(methodOverride("_method")); //override method=DELETE or method=PUT
+app.use(flash()); //flash messages
+
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "There you go",
+    resave: false,
+    saveUninitialized: false
+})); //express-session
+app.use(passport.initialize());
+app.use(passport.session());
+// passport.use(new LocalStrategy(User.Authenticate()));
+// passport.serializeUser(User.serializeUser);
+// passport.deserializeUser(User.deserializeUser);
+
+//STORE LOCAL VARIABLES
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = 
+})
+    
